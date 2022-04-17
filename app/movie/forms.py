2a55b55af9117c199f7cb0d92abf291959c15727
Derek Wan <d.wan@icloud.com>
2022-04-17 18:08:45 +0800

    Finish chapter 25 (#8)
    * Update
    
    * rename
    
    * add
    
    * cleaner imports
    
    * Use / and *
    
    * cleaner imports
    
    * Add reviews

---
+++ app/movie/forms.py
@@ -0,0 +1,22 @@
+from typing import Any
+from typing import TYPE_CHECKING
+
+from django.forms import ModelForm
+from django.forms import Textarea
+from movie.models import Review
+
+
+class ReviewForm(ModelForm[Review] if TYPE_CHECKING else ModelForm):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        for name, cls in [
+            ("text", "form-control"),
+            ("watch_again", "form-check-input"),
+        ]:
+            self.fields[name].widget.attrs.update({"class": cls})
+
+    class Meta:  # type: ignore
+        model = Review
+        fields = ["text", "watch_again"]
+        labels = {"watch_again": "Watch Again"}
+        widgets = {"text": Textarea(attrs={"rows": 4})}
--- app/movie/views.py
+++ app/movie/views.py
@@ -1,11 +1,16 @@
+from typing import Any
+
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.shortcuts import render
+from movie.forms import ReviewForm
 from movie.models import Movie
+from movie.models import Review
 
 
-def home(request: HttpRequest) -> HttpResponse:
+def home(request: HttpRequest, /) -> HttpResponse:
     if search_term := request.GET.get("search_movie"):
         movies = Movie.objects.filter(title__icontains=search_term)
     else:
@@ -15,15 +20,33 @@ def home(request: HttpRequest) -> HttpResponse:
     )
 
 
-def about(_: HttpRequest) -> HttpResponse:
+def about(_: HttpRequest, /) -> HttpResponse:
     return HttpResponse("<h1>Welcome to About Page</h1>")
 
 
-def signup(request: HttpRequest) -> HttpResponse:
+def signup(request: HttpRequest, /) -> HttpResponse:
     email = request.GET.get("email")
     return render(request, "signup.html", {"email": email})
 
 
-def detail(request: HttpRequest, movie_id: int) -> HttpResponse:
-    movie = get_object_or_404(Movie, pk=movie_id)
-    return render(request, "detail.html", {"movie": movie})
+def detail(request: HttpRequest, /, *, movie_pk: int) -> HttpResponse:
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    reviews = Review.objects.filter(movie=movie)
+    return render(request, "detail.html", {"movie": movie, "reviews": reviews})
+
+
+def create_review(request: HttpRequest, /, *, movie_pk: int) -> HttpResponse:
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    context: dict[str, Any] = {"form": ReviewForm, "movie": movie}
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        try:
+            review = form.save(commit=False)
+        except ValueError:
+            context["error"] = "bad data passed in"
+        else:
+            review.user = request.user
+            review.movie = movie
+            review.save()
+            return redirect("detail", review.movie.pk)
+    return render(request, "create_review.html", context)
