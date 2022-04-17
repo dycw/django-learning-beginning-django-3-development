7b43e8b031c036881622ebd6d96891231bd02063
Derek Wan <d.wan@icloud.com>
2022-04-17 15:55:44 +0800

    fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "app" / "static"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
