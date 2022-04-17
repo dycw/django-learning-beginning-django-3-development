f20ae448ac883f16e897345c16978c5df8b381b8
Derek Wan <d.wan@icloud.com>
2022-04-17 22:26:00 +0800

    Betterr/

--- app/app/settings.py
+++ app/app/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "app/static"]
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "static"
 
-MEDIA_ROOT = BASE_DIR / "app/media"
+MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
 
 # Default primary key field type
