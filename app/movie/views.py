fb08b7a8f81cb09c62ddcee3f453215f568a4d59
Derek Wan <d.wan@icloud.com>
2022-04-17 17:49:44 +0800

    add

--- app/movie/views.py
+++ app/movie/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.shortcuts import render
+from movie.forms import ReviewForm
 from movie.models import Movie
 
 
@@ -31,4 +33,12 @@ def detail(request: HttpRequest, movie_id: int) -> HttpResponse:
 
 def create_review(request: HttpRequest, movie_id: int) -> HttpResponse:
     movie = get_object_or_404(Movie, pk=movie_id)
-    return render(request, "detail.html", {"movie": movie})
+    context = {"movie": movie}
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.user = request.user
+        review.movie = movie
+        review.save()
+        return redirect("detail", review.movie.pk)
+    return render(request, "create_review.html", context)
