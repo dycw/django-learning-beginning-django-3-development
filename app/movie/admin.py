ab7d7d692764dee4beedcae8315fa4225e05f2a3
Derek Wan <d.wan@icloud.com>
2022-04-17 17:31:21 +0800

    Update

--- app/movie/admin.py
+++ app/movie/admin.py
@@ -1,5 +1,7 @@
 from django.contrib.admin import site
 from movie.models import Movie
+from movie.models import Review
 
 
 site.register(Movie)
+site.register(Review)
--- app/movie/models.py
+++ app/movie/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import User
+from django.db.models import BooleanField
+from django.db.models import CASCADE
 from django.db.models import CharField
+from django.db.models import DateTimeField
+from django.db.models import ForeignKey
 from django.db.models import ImageField
 from django.db.models import Model
 from django.db.models import URLField
@@ -9,3 +14,14 @@ class Movie(Model):
     description = CharField(max_length=250)
     image = ImageField(upload_to="movie/images/")
     url = URLField(blank=True)
+
+
+class Review(Model):
+    text = CharField(max_length=100)
+    date = DateTimeField(auto_now_add=True)
+    user = ForeignKey(User, on_delete=CASCADE)
+    movie = ForeignKey(Movie, on_delete=CASCADE)
+    watch_again = BooleanField()
+
+    def __str__(self) -> str:
+        return self.text
