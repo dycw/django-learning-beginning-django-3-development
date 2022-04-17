519a6d06be2b6643537a37ac86e1e589efa7390b
Derek Wan <d.wan@icloud.com>
2022-04-17 14:49:39 +0800

    chp8

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,9 +2,11 @@ from django.contrib.admin import site
 from django.urls import path
 from movie.views import about
 from movie.views import home
+from movie.views import signup
 
 urlpatterns = [
     path("admin/", site.urls),
     path("", home, name="home"),
     path("about/", about, name="about"),
+    path("signup/", signup, name="signup"),
 ]
--- app/movie/views.py
+++ app/movie/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "home.html", {"name": "Greg Lim"})
+    search_term = request.GET.get("search_movie")
+    return render(request, "home.html", {"search_term": search_term})
 
 
 def about(_: HttpRequest) -> HttpResponse:
     return HttpResponse("<h1>Welcome to About Page</h1>")
+
+
+def signup(request: HttpRequest) -> HttpResponse:
+    email = request.GET.get("email")
+    return render(request, "signup.html", {"email": email})
