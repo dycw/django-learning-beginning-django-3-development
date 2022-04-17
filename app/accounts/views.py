ad6c7f7bb7eda8a077e3237b939e84c7417bb951
Derek Wan <d.wan@icloud.com>
2022-04-17 16:50:55 +0800

    Finish chapter 20 (#7)
    * Update
    
    * More
    
    * impl login

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,8 +1,56 @@
-from django.contrib.auth.forms import UserCreationForm
+from typing import Any
+
+from accounts.forms import UserCreateForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 
 def signupaccount(request: HttpRequest) -> HttpResponse:
-    return render(request, "signupaccount.html", {"form": UserCreationForm})
+    context: dict[str, Any] = {"form": UserCreateForm}
+    if request.method == "POST":
+        post = request.POST
+        if (password := post["password1"]) == post["password2"]:
+            try:
+                user = User.objects.create_user(  # type: ignore
+                    post["username"], password=password
+                )
+            except IntegrityError:
+                context[
+                    "error"
+                ] = "Username already taken. Choose new username."
+            else:
+                user.save()
+                login(request, user)
+                return redirect("home")
+        else:
+            context["error"] = "Passwords do not match"
+    return render(request, "signupaccount.html", context)
+
+
+def logoutaccount(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect("home")
+
+
+def loginaccount(request: HttpRequest) -> HttpResponse:
+    context: dict[str, Any] = {"form": AuthenticationForm}
+    if request.method == "POST":
+        post = request.POST
+        if (
+            user := authenticate(
+                request, username=post["username"], password=post["password"]
+            )
+        ) is None:
+            context["error"] = "User name and password do not match"
+        else:
+            login(request, user)
+            return redirect("home")
+    return render(request, "loginaccount.html", context)
