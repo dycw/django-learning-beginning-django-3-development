440deb032de4976bb0b702a3c9b636c196e8a085
Derek Wan <d.wan@icloud.com>
2022-04-17 14:34:08 +0800

    fix

--- app/movie/views.py
+++ app/movie/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpRequest
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, "home.html", {"name": "Greg Lim"})
+
+
+def about(_: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Welcome to About Page</h1>")
